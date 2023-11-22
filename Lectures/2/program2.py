1a956f04c88328ed84c7171106876b20821fb703
AbhishekASLK <bhoslea925@gmail.com>
2023-11-22 12:29:57 +0530

    Lecture-2

---
+++ Lectures/2/program2.py
@@ -0,0 +1,68 @@
+# Data Types
+
+a = 10;
+print(a)
+
+# In python2 we have long data type to represent the large integral value
+# but in python3 int can store whatever large integral value stored by long
+
+# Decimal Form
+a = 5;
+print(a);
+
+# Octal Form
+a = 0o7
+print(a);
+
+# Binary Form
+a = 0B1111
+print(a)
+
+# Hexadecimal Form
+a = 0xff
+print(a)
+
+print("--------------")
+
+z = 50;
+print(z);
+
+float = 10
+print(float)
+
+int = 20
+print(int)
+
+str = "Shashi"
+print(str)
+
+f = 1.2e10
+print(f)
+
+a = True
+print(a)
+
+print(False+True)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
