6726b8a8fe27a49d9082d84e765d699236a06880
Abhishek Bhosle <bhoslea925@gmail.com>
2024-01-18 16:26:10 +0530

    class inherited from ABC can make object until it not has abstract method

---
+++ Lectures/35/program9.py
@@ -0,0 +1,9 @@
+from abc import ABC
+class Demo(ABC):
+    def fun(self):
+        print('fun')
+
+Demo()
+
+# Demo class ha abstract class aahe but tyamadhe ek pn
+# abstract method nahi so object banau shakto
