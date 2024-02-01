52870dbeefd660556fe31ba9c49bd8ea35a4dadf
Abhishek Bhosle <bhoslea925@gmail.com>
2024-02-01 21:53:08 +0530

    tkinter initialized

---
+++ Projects/TKinter/snippets/07_frame.py
@@ -0,0 +1,22 @@
+# ================= padx and ipadx ===================
+
+import tkinter as tk
+from tkinter import ttk
+
+# Window
+root = tk.Tk()
+root.title('Frame')
+root.geometry('400x600')
+
+# Widgets
+label1 = ttk.Label(text='Label 1',background='red')
+label2 = ttk.Label(text='Label 2',background='blue')
+label3 = ttk.Label(text='Label 3',background='green')
+label3 = ttk.Label(text='Label 3',background='orange')
+button = ttk.Button(text='Button1')
+button = ttk.Button(text='Button2')
+
+# layout
+
+
+root.mainloop()
